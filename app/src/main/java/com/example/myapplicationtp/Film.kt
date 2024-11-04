17d3eceb96ba042603e0093495fc0aecc02d41a3
maevadesfours <maeva.desfours@orange.fr>
2024-11-04 11:55:24 +0100

    today

--- app/src/main/java/com/example/myapplicationtp/Film.kt
+++ app/src/main/java/com/example/myapplicationtp/Film.kt
@@ -1,7 +1,9 @@
 package com.example.myapplicationtp
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -12,14 +14,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import androidx.window.core.layout.WindowSizeClass
 import androidx.window.core.layout.WindowWidthSizeClass
 import com.example.myapplicationtp.ui.theme.MyApplicationTPTheme
+import com.example.myapplicationtp.ui.theme.MyBlue
+import com.example.myapplicationtp.ui.theme.MyGrey
 
 import java.nio.file.WatchEvent
 
@@ -55,6 +62,7 @@ fun FilmsScreen(
                      HomeHorizontal("Android")
                  }
              }*/
+
     }
 
 }
@@ -62,12 +70,34 @@ fun FilmsScreen(
 @Composable
 fun MovieItem(movie: UnFilm,
               navController: NavController) {
-
-    Text(
-        text = movie.title
-    )
-
+    Column (Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center){
+    Titre(movie, navController)
+    Date(movie, navController)
+    }
 }
+
+@Composable
+fun Titre(movie: UnFilm,
+         navController: NavController){
+    Text(
+        text = movie.title,
+        color = MyBlue,
+        fontWeight = FontWeight.Bold,
+        fontSize = 15.sp
+    )
+}
+
+@Composable
+fun Date(movie: UnFilm,
+         navController: NavController){
+    Text(
+        text = movie.release_date,
+        color = MyGrey,
+        fontSize = 10.sp
+    )
+}
+
+
 
 
 
